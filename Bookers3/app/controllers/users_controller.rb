a03898602eabe68d2ce1e3d19c13a6f2b5650571
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-16 05:56:41 +0000

    [Add]3

--- Bookers3/app/controllers/users_controller.rb
+++ Bookers3/app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  
+  def index
+    
+  end
+  
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
